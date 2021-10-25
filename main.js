d496d8a27c0eac933bed0e953f27bd0486e9fbe0
Karen Samirah <karen.samirah@outlook.com>
2021-10-24 22:09:35 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'KarenSamirah',
+  linkedin: 'karen-samirah',
+  instagram: 'karen.samirah',
+  youtube: 'channel/UC1ID5rgaMIgdIXsZyW8vaAQ'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGitHub.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
